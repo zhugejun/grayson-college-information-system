4030d8e90459cde71f9a67344f2cc21646af851c
zhugejun <gejun.anderson@gmail.com>
2020-05-15 14:34:23 -0500

    add static root

--- grayson_college_information_system/settings.py
+++ grayson_college_information_system/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'grayson_college_information_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'grayson',
-            'USER': 'zhua',
-            'PASSWORD': 'buzhidao+2659',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'grayson',
+#         'USER': 'zhua',
+#         'PASSWORD': 'buzhidao+2659',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -136,9 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # time and date input format
