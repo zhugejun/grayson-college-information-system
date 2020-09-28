1f5c34e096a0ae0a18d30c24a4623c2f441eb782
zhugejun <gejun.anderson@gmail.com>
2020-09-27 22:45:47 -0500

    changed admin views for course

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -6,7 +6,13 @@ from .models import Course, Campus, Instructor, Location, Term, Schedule
 admin.site.register(Term)
 
 
-admin.site.register(Course)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'number', 'credit', 'name')
+    list_filter = ('subject', )
+    ordering = ('subject', 'number')
+
+
 admin.site.register(Location)
 admin.site.register(Instructor)
 admin.site.register(Campus)
