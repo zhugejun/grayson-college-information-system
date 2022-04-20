d2d71b7cc07b8c600fae8f6595d06fbf271a0567
Gejun Zhu <gejun.anderson@gmail.com>
2022-04-19 21:23:55 -0500

    remove unused packages

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,4 @@ urlpatterns = [
     path('login/', views.login_request, name="login"),
     path('logout/', views.logout_request, name="logout"),
     path('account/', views.account, name="account"),
-
-    path('select2/', include('select2.urls')),
 ]
