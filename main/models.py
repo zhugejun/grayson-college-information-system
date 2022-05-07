2f5fe785d1605efc4ed46646ece4bf378c59d523
Gejun Zhu <gejun.anderson@gmail.com>
2022-05-07 18:13:11 -0500

    add default group permissions to new users

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
 
 
 class Profile(models.Model):
@@ -9,7 +10,7 @@ class Profile(models.Model):
     subjects = models.CharField(max_length=512, null=True, blank=True)
 
     def __str__(self):
-        return f'<Profile {self.user.username}>'
+        return f'Profile<{self.user.username}>'
 
 # save user_id to profile when user was created
 @receiver(post_save, sender=User)
@@ -18,3 +19,10 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         instance.profile.save()
+
+@receiver(post_save, sender=User)
+def add_to_default_group(sender, instance, created, **kwargs):
+    if created:
+        view_groups = Group.objects.filter(name__contains="view")
+        for grp in view_groups:
+            instance.groups.add(grp)
