ec372d439dee47831b70156ea8ca43689ef9fbbb
Gejun Zhu <gejun.anderson@gmail.com>
2022-11-29 09:50:49 -0600

    fix img on home page

--- grayson_college_information_system/urls.py
+++ grayson_college_information_system/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('scheduling/', include('scheduling.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
